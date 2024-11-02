f6943557e3428ec7884894a5ccf4e67e567e9b8b
幻弦让叶 <1248389474@qq.com>
2024-11-02 21:46:21 +0800

    opt: load pic

--- app/src/main/java/com/lanier/game3/client/BaseApplication.kt
+++ app/src/main/java/com/lanier/game3/client/BaseApplication.kt
@@ -1,6 +1,9 @@
 package com.lanier.game3.client
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
 import com.lanier.game3.client.net.APIRequester
 
 /**
@@ -8,10 +11,22 @@ import com.lanier.game3.client.net.APIRequester
  * Author:  幻弦让叶
  * Date:    2024/10/30 22:55
  */
-class BaseApplication : Application() {
+class BaseApplication : Application(), ImageLoaderFactory {
 
     override fun onCreate() {
         super.onCreate()
         APIRequester.initAPI()
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .respectCacheHeaders(false)
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(cacheDir.resolve("img_cache"))
+                    .maxSizePercent(0.02)
+                    .build()
+            }
+            .build()
     }
 }
