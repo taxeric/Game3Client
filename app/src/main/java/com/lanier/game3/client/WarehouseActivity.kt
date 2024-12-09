887dab332a05cf0abe7307bccd223d82e0fdf970
幻弦让叶 <1248389474@qq.com>
2024-12-10 02:03:06 +0800

    feat: warehouse #1

---
+++ app/src/main/java/com/lanier/game3/client/WarehouseActivity.kt
@@ -0,0 +1,26 @@
+package com.lanier.game3.client
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.lanier.game3.client.databinding.ActivityWarehouseBinding
+
+class WarehouseActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityWarehouseBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
